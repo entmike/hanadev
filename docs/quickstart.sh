1a6d5f41bf296cdb59ad196ebf60b43e5ef14708
Mike Howles <entmike@gmail.com>
2019-05-21 08:38:12 -0500

    Update quickstart.sh

--- docs/quickstart.sh
+++ docs/quickstart.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+bash <(curl -s https://entmike.github.io/hanadev/resize.sh) 20
+git clone https://github.com/entmike/hanadev/tree/Part4
