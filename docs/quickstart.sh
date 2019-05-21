df7dfa661786afc257777d5a05bedea21b7044c8
Mike Howles <mike.howles@gmail.com>
2019-05-21 08:47:04 -0500

    Update quickstart.sh

--- docs/quickstart.sh
+++ docs/quickstart.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/entmike/hanadev/tree/Part4
+git clone -b Part4 https://github.com/entmike/hanadev.git
+cd hanadev
+wget https://entmike.github.io/hanadev/.env
