4fcbb61fda67d15da0f4d3e8a844d050aa8cc96b
Mike Howles <mike.howles@gmail.com>
2019-05-30 15:14:48 +0000

    Authentication Part

--- runhxe.sh
+++ runhxe.sh
@@ -6,7 +6,9 @@
 docker run --rm --env-file=./.env \
     --name hxe \
     --hostname hxe \
-    -v hanadev_hana-express:/hana/mounts \
+    -v hxedev:/hana/mounts \
     -v $HOME/environment/hanadev/db-scripts:/scripts \
     -p 39017:39017 \
-    store/saplabs/hanaexpress:2.00.036.00.20190223.1
+    store/saplabs/hanaexpress:2.00.036.00.20190223.1 \
+    --agree-to-sap-license \
+    --master-password HXEHana1
