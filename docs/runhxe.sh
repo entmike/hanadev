37ff1121d7957819a71d4c4de6f2d6361e39650e
Mike Howles <mike.howles@gmail.com>
2019-05-30 11:25:34 -0500

    Add tenant port (39041)

--- docs/runhxe.sh
+++ docs/runhxe.sh
@@ -6,4 +6,5 @@ docker run --rm --env-file=./.env \
     -v hanadev_hana-express:/hana/mounts \
     -v $HOME/environment/hanadev/db-scripts:/scripts \
     -p 39017:39017 \
+    -p 39041:39041 \
     store/saplabs/hanaexpress:2.00.036.00.20190223.1
